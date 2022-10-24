360fb3be7da0f49db5b420be1d8fb22ef557700d
ahmedfayazyousuf <ahmedfayazyousuf@gmail.com>
2022-10-24 11:51:36 +0400

    a
    a

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const authenticate = require("./middleware/authenticate");
 var bodyParser = require('body-parser')
-var cors = require('cors')
+const cors = require('cors');
 
 app.use(cors());
 
